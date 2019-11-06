349d7c3232ae20b0e308276969b4b665bc315425
Alberto402 <albegu02@gmail.com>
2019-11-06 20:06:24 +0100

    Respuesta login añadida

--- back-end/loginTutor.php
+++ back-end/loginTutor.php
@@ -1,24 +1,29 @@
 <?php
+
 session_start();
 require_once 'db_function.php';
+
 $db = new DBFunctions();
 $response = array("error" => FALSE);
-if (isset($_POST['contrasena']) && isset($_POST['email'])) {
 
+if (isset($_POST['contrasena']) && isset($_POST['email'])) {
     $email = $_POST['email'];
     $contra = $_POST['contrasena'];
     $user = $db->getTutorEmailPass($email, $contra);
     if ($user != FALSE) {
-
-        $_SESSION['login']=True;
-        $_SESSION['tutorId'] = $user['IdTutor'];
-        //falta hablar que se va a devolver
-        /*
+        
+        $_SESSION['login'] = True;
+        $_SESSION['tutorId'] = $user['Id_tutor'];
+        
         $response["error"] = FALSE;
-        $response["user"]["Usuario"] = $user["Usuario"];
-        $response["user"]["email"] = $user["email"];
-        $response["user"]["telefono"] = $user["telefono"];
-        echo json_encode($response);*/
+        $response["tutor"]["idTutor"] = $user["IdTutor"];
+        $response["tutor"]["email"] = $user["Email"];
+        $response["tutor"]["nombre"] = $user["Nombre"];
+        $response["tutor"]["apellidos"] = $user["Apellidos"];
+        $response["tutor"]["fechaNacimiento"] = $user["FechaNacimiento"];
+        $response["tutor"]["tipoTutor"] = $user["TipoTutor"];
+        echo json_encode($response);
+       
     } else {
         $response["error"] = TRUE;
         $response["error_msg"] = "usuario o contrasena incorrectos";
