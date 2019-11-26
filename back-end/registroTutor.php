ce99c1fbfec9fc3585c67abac19e397ab520d53f
RaulMurillo <ramurimontero@gmail.com>
2019-11-26 15:23:31 +0100

    Solved errors in registroTutor

--- back-end/registroTutor.php
+++ back-end/registroTutor.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'db_function.php';
 $db = new DBFunctions();
-//$response = array("error" => false);
+$response = array("error" => false);
 
 // define variables and set to empty values
 $name = $email = $surname = $password = $cpassword = "";
@@ -9,36 +9,57 @@ $name = $email = $surname = $password = $cpassword = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //Validates User name
     if (empty($_POST["name"])) {
-        throw new Exception("Introduce a user name, please");
+        //throw new Exception("Introduce a user name, please");
+        $response["error"] = true;
+        $response["error_msg"] = "Introduce a user name, please";
+        echo json_encode($response);
     } else {
         $name = test_input($_POST["name"]);
         // check if name only contains letters and whitespace
         if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
-            throw new Exception("Only letters and white space allowed");
+            //throw new Exception("Only letters and white space allowed");
+            $response["error"] = true;
+            $response["error_msg"] = "Introduce a user name, please";
+            echo json_encode($response);
         }
     }
     //Validates User surname
     if (empty($_POST["surname"])) {
-        throw new Exception("Introduce a user surname, please");
+        //throw new Exception("Introduce a user surname, please");
+        $response["error"] = true;
+        $response["error_msg"] = "Introduce a user surname, please";
+        echo json_encode($response);
     } else {
         $surname = test_input($_POST["surname"]);
         // check if name only contains letters and whitespace
         if (!preg_match("/^[a-zA-Z ]*$/", $surname)) {
-            throw new Exception("Only letters and white space allowed");
+            //throw new Exception("Only letters and white space allowed");
+            $response["error"] = true;
+            $response["error_msg"] = "Only letters and white space allowed as user name";
+            echo json_encode($response);
         }
     }
     //Validates email
     if (empty($_POST["email"])) {
-        throw new Exception("Email is required");
+        //throw new Exception("Email is required");
+        $response["error"] = true;
+        $response["error_msg"] = "Email is required";
+        echo json_encode($response);
     } else {
         $email = test_input($_POST["email"]);
         // check if e-mail address is well-formed
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            throw new Exception("Invalid email format");
+            //throw new Exception("Invalid email format");
+            $response["error"] = true;
+            $response["error_msg"] = "Introduce a valid email, please";
+            echo json_encode($response);
         }
         // check if e-mail already exists
         if ($db->existeEmail($email)) {
-            throw new Exception("This e-mail already exists");
+            //throw new Exception("This e-mail already exists");
+            $response["error"] = true;
+            $response["error_msg"] = "This e-mail already exists";
+            echo json_encode($response);
         }
     }
     //Validates password & confirm passwords.
@@ -46,18 +67,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $password = test_input($_POST["password"]);
         //$cpassword = test_input($_POST["cpassword"]);
         if (strlen($_POST["password"]) < '8') {
-            throw new Exception("Your password must contain at least 8 characters");
+            //throw new Exception("Your password must contain at least 8 characters");
+            $response["error"] = true;
+            $response["error_msg"] = "Your password must contain at least 8 characters";
+            echo json_encode($response);
         } elseif (!preg_match("#[0-9]+#", $password)) {
-            throw new Exception("Your password must contain at least 1 number");
+            //throw new Exception("Your password must contain at least 1 number");
+            $response["error"] = true;
+            $response["error_msg"] = "Your password must contain at least 1 number";
+            echo json_encode($response);
         } elseif (!preg_match("#[A-Z]+#", $password)) {
-            throw new Exception("Your password must contain at least 1 capital letter");
+            //throw new Exception("Your password must contain at least 1 capital letter");
+            $response["error"] = true;
+            $response["error_msg"] = "Your password must contain at least 1 capital letter";
+            echo json_encode($response);
         } elseif (!preg_match("#[a-z]+#", $password)) {
-            throw new Exception("Your password must contain at least 1 lowercase letter");
+            //throw new Exception("Your password must contain at least 1 lowercase letter");
+            $response["error"] = true;
+            $response["error_msg"] = "Your password must contain at least 1 lowercase letter";
+            echo json_encode($response);
         }
     } elseif (!empty($_POST["password"])) {
-        throw new Exception("Please check you've entered or confirmed your password");
+        //throw new Exception("Please check you've entered or confirmed your password");
+        $response["error"] = true;
+        $response["error_msg"] = "Please check you've entered or confirmed your password";
+        echo json_encode($response);
     } else {
-        throw new Exception("Please enter password");
+        //throw new Exception("Please enter password");
+        $response["error"] = true;
+        $response["error_msg"] = "Please enter password";
+        echo json_encode($response);
     }
     // Create DB entry.
     if (isset($_POST['birth'])) {
@@ -66,7 +105,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $user = $db->insertTutor($name, $surname, $email, $password);
     }
-    echo json_encode($user);
+    $response["user"] = $user;
+    echo json_encode($response);
 }
 /*Each $_POST variable with be checked by the function*/
 function test_input($data)
