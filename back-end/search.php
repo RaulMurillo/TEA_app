73e803c3ed79fa45d815290a4df4f9c57f808228
Alberto Gutiérrez <39036564+alberto402@users.noreply.github.com>
2019-11-29 12:38:48 +0100

    Actualizada devolucion busqueda, para que devuelva array de objetos path y nombre de archivo

--- back-end/search.php
+++ back-end/search.php
@@ -3,7 +3,8 @@ require_once 'db_function.php';
 require_once 'pgregg.php';
 $db = new DBFunctions();
 $response = array("error" => false);
-$result=array();
+$result=[];
+$info=array();
 ;
 if (isset($_POST['palabra'])) {
     $palabra=$_POST['palabra'];
@@ -11,12 +12,17 @@ if (isset($_POST['palabra'])) {
    // $files = glob($directorio."/$palabra*.jpg");
     $files = glob($directorio."/*");
     foreach ($files as $file){
-        if(($aux=stristr($file, $palabra))!=false)
-           array_push($result,$file);
+        if(($aux=stristr($file, $palabra))!=false){
+            $iparr = explode("/", $file); 
+            $name= explode(".", $iparr[2]); 
+            $result["nombre"] = $name[0];
+            $result["path"] = $file;
+            array_push($info,$result);
+        }
             
     }
     $response["error"] = false;
-    $response["sources"] = $result;
+    $response["sources"]=$info;
     echo json_encode($response,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES );
 
 }
